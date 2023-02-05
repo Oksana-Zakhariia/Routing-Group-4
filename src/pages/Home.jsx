11b38d5fbfde62c4fabacc3d47e7de3bd181b0a4
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-02-05 11:57:23 +0200

    cyprus

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { getCountries, fetchCountry, fetchByRegion } from '../service/country-service'
+import { useEffect, useState } from 'react';
+
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    getCountries().then(data => { setCountries(data); })
+    
+  }, []) 
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
